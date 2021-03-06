747e8ea2a28a41083994aed3ff6e4d52aed3c44a
Epap <kathy@lanit.ru>
2021-03-06 17:56:45 +0300

    Second commit

--- src/model/Application.java
+++ src/model/Application.java
@@ -4,12 +4,15 @@ public class Application {
     public static void main(String[] args)
     {
         Kotik kotik = new Kotik();
-        kotik.setKotik(3,"Барсик",3,"Мяу-Мяу");
+        kotik.setKotik(3,"Барсик",3,"Мииук");
         Kotik kotik1;
-        kotik1 = new Kotik(2,"Тигрик", 1,"Мииук");
+        kotik1 = new Kotik(2,"Тигрик", 1,"Мяу-Мяу");
         System.out.println("Имя котика "+ kotik1.getName()+"  Вес котика равен " + kotik1.getWeight() + "кг");
         System.out.println("Так мяукуют котики:" + kotik1.getMeow()+" " + kotik.getMeow()  );
-        if (kotik1.getName().compareTo(kotik.getMeow()) == 0) {
+       String str1 = kotik1.getMeow();
+       String str2 = kotik.getMeow();
+        // if (kotik1.getName().equals(kotik.getMeow())) {
+        if (str1.equals(str2)) {
             System.out.println("Это одинаковые Мяу!");
         } else {System.out.println("Это разные Мяу!");
 
